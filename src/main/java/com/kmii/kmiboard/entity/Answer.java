29e5f0b28320f83eb6864261ee3aa4e2a8a6079f
ckm15 <ckm15@mi>
2025-09-08 05:15:02 +0900

    DB변경 : H2 -> Oracle

--- src/main/java/com/kmii/kmiboard/entity/Answer.java
+++ src/main/java/com/kmii/kmiboard/entity/Answer.java
@@ -10,6 +10,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,17 +24,24 @@ import lombok.Setter;
 
 
 @Entity
+@Table(name = "answer") //실제로 매핑될 데이터베이스의 테이블 이름 설정
+@SequenceGenerator(
+		name = "ANSWER_SEQ_GENERATOR", //JPA 내부 시퀀스 이름
+		sequenceName = "ANSWER_SEQ", //실제 DB 시퀀스 이름 
+		initialValue = 1, //시퀀스 시작값
+		allocationSize = 1 //시퀀스 증가치
+		)
 public class Answer {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ANSWER_SEQ_GENERATOR")
 	private Integer id; //기본키, 자동증가
 	
-	@Column(columnDefinition = "TEXT")
+	@Column(length = 500)
 	private String content;
 	
 	
-	private LocalDateTime createDate;
+	private LocalDateTime createdate;
 	
 	//N:1 -> 답변들: 질문 ->@ManyToOne
 	@ManyToOne
--- src/test/java/com/kmii/kmiboard/Test01.java
+++ src/test/java/com/kmii/kmiboard/Test01.java
@@ -1,7 +1,11 @@
 package com.kmii.kmiboard;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,24 +19,34 @@ public class Test01 {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
+//	@Test
+//	@DisplayName("question 테이블에 질문글 저장하기")
+//	public void testJpa1() {
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무엇인가요?"); //질문 제목
+//		q1.setContent("sbb에 대해 알고 싶습니다"); // 질문 내용
+//		q1.setCreatedate(LocalDateTime.now()); // 현재 시간 넣기
+//		//q1 -> entity 생성 완료
+//		questionRepository.save(q1);
+//		
+//		
+//		Question q2 = new Question();
+//		q2.setSubject("스프링부트 모델이 무엇인가요?"); //질문 제목
+//		q2.setContent("id는 자동 생성되는게 맞나요"); // 질문 내용
+//		q2.setCreatedate(LocalDateTime.now()); // 현재 시간 넣기
+//		//q1 -> entity 생성 완료
+//		questionRepository.save(q2);		
+//		
+//		
+//	}
+	
 	@Test
-	public void testJpa1() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?"); //질문 제목
-		q1.setContent("sbb에 대해 알고 싶습니다"); // 질문 내용
-		q1.setCreateDate(LocalDateTime.now()); // 현재 시간 넣기
-		//q1 -> entity 생성 완료
-		questionRepository.save(q1);
-		
-		
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델이 무엇인가요?"); //질문 제목
-		q2.setContent("id는 자동 생성되는게 맞나요"); // 질문 내용
-		q2.setCreateDate(LocalDateTime.now()); // 현재 시간 넣기
-		//q1 -> entity 생성 완료
-		questionRepository.save(q2);		
-		
+	@DisplayName("모든 질문글 조회하기")
+	public void testJpa2() {
+		List<Question> allQuestion = questionRepository.findAll(); //모든 질문글 조회하기 , findAll은 repository에 따로 만들지 않아도됨
+		assertEquals(2, allQuestion.size());  // 예상 결과 확인하기
 		
 	}
+	
 
 }
