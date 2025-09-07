6e74141b11cb9bc8d1508c0564e23d6695b69544
ckm15 <ckm15@mi>
2025-09-08 05:22:42 +0900

    테스트 추가

--- src/test/java/com/kmii/kmiboard/Test01.java
+++ src/test/java/com/kmii/kmiboard/Test01.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,11 +42,24 @@ public class Test01 {
 //	}
 	
 	@Test
-	@DisplayName("모든 질문글 조회하기")
+	@DisplayName("모든 질문글 조회하기 테스트")
 	public void testJpa2() {
 		List<Question> allQuestion = questionRepository.findAll(); //모든 질문글 조회하기 , findAll은 repository에 따로 만들지 않아도됨
-		assertEquals(2, allQuestion.size());  // 예상 결과 확인하기
+		assertEquals(2, allQuestion.size());  // 예상 결과 확인하기(기대값, 실제값) : 같지 않으면 test 실패
 		
+		Question question = allQuestion.get(0); //첫번째 질문글 가져오기
+		assertEquals("sbb가 무엇인가요?", question.getSubject());
+		
+	}
+	
+	@Test
+	@DisplayName("질문 글 번호(기본키인 id)로 조회 테스트")
+	public void testJpa3() {
+		Optional<Question> qOptional = questionRepository.findById(1); //기본키로 조회 : 1번 글 가져오기
+		if(qOptional.isPresent()) { //참이면 기본키 번호가 존재
+			Question question = qOptional.get(); // 글 꺼내기
+			assertEquals("sbb가 무엇인가요?", question.getSubject());
+		}
 	}
 	
 
