3a3f9bf2deebc47ba24d753dd933aa227fcbc706
ckm15 <ckm15@mi>
2025-09-08 04:33:26 +0900

    Repository 추가

---
+++ src/main/java/com/kmii/kmiboard/repository/QuestionRepository.java
@@ -0,0 +1,11 @@
+package com.kmii.kmiboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kmii.kmiboard.entity.Question;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+	
+	
+
+}
